d35df5ba95c53f84b4882b4603ef7c2f2f28814d
funzter <cavehive@protonmail.com>
2022-07-14 19:55:02 +0200

    game system changed

--- src/main/java/de/timesnake/game/push/server/PushGame.java
+++ src/main/java/de/timesnake/game/push/server/PushGame.java
@@ -1,22 +1,22 @@
 package de.timesnake.game.push.server;
 
 import de.timesnake.basic.bukkit.util.exceptions.UnsupportedGroupRankException;
-import de.timesnake.basic.game.util.Game;
 import de.timesnake.basic.game.util.Kit;
 import de.timesnake.basic.game.util.Team;
-import de.timesnake.database.util.game.DbGame;
+import de.timesnake.basic.game.util.TmpGame;
 import de.timesnake.database.util.game.DbKit;
 import de.timesnake.database.util.game.DbMap;
 import de.timesnake.database.util.game.DbTeam;
+import de.timesnake.database.util.game.DbTmpGame;
 import de.timesnake.game.push.map.PushMap;
 import de.timesnake.game.push.user.PushKit;
 
-public class PushGame extends Game {
+public class PushGame extends TmpGame {
 
     private static final String BLUE_NAME = "blue";
     private static final String RED_NAME = "red";
 
-    public PushGame(DbGame game) {
+    public PushGame(DbTmpGame game) {
         super(game, true);
     }
 
