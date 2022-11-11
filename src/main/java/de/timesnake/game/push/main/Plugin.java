2b1e0385a9d8df061e3b3b58abeb9e6dbecfa875
funzter <cavehive@protonmail.com>
2022-11-11 20:31:16 +0100

    add logger to plugins, add log command

--- src/main/java/de/timesnake/game/push/main/Plugin.java
+++ src/main/java/de/timesnake/game/push/main/Plugin.java
@@ -1,5 +1,5 @@
 /*
- * game-push.main
+ * timesnake.game-push.main
  * Copyright (C) 2022 timesnake
  *
  * This program is free software; you can redistribute it and/or
@@ -18,11 +18,16 @@
 
 package de.timesnake.game.push.main;
 
+import de.timesnake.library.basic.util.LogHelper;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Plugin extends de.timesnake.basic.loungebridge.util.chat.Plugin {
 
-    public static final Plugin PUSH = new Plugin("Push", "GPU");
+    public static final Plugin PUSH = new Plugin("Push", "GPU", LogHelper.getLogger("Push", Level.INFO));
 
-    protected Plugin(String name, String code) {
-        super(name, code);
+    protected Plugin(String name, String code, Logger logger) {
+        super(name, code, logger);
     }
 }
