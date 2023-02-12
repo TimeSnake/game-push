64098572542ab464fa8d8981c400c0153a0072e0
funzter <cavehive@protonmail.com>
2023-02-12 23:20:57 +0100

    minor

--- src/main/java/de/timesnake/game/push/user/PushUser.java
+++ src/main/java/de/timesnake/game/push/user/PushUser.java
@@ -7,12 +7,11 @@ package de.timesnake.game.push.user;
 import de.timesnake.basic.bukkit.util.user.inventory.ExItemStack;
 import de.timesnake.basic.loungebridge.util.user.GameUser;
 import de.timesnake.game.push.server.PushServer;
+import java.util.List;
 import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.EquipmentSlot;
-
-import java.util.List;
 
 public class PushUser extends GameUser {
 
@@ -21,10 +20,14 @@ public class PushUser extends GameUser {
                     new ExItemStack(1, Material.BOW).setUnbreakable(true),
                     new ExItemStack(7, Material.ARROW).asQuantity(8),
                     new ExItemStack(8, Material.BEEF).asQuantity(8),
-                    new ExItemStack(Material.CHAINMAIL_HELMET).setUnbreakable(true).setSlot(EquipmentSlot.HEAD),
-                    new ExItemStack(Material.GOLDEN_CHESTPLATE).setUnbreakable(true).setSlot(EquipmentSlot.CHEST),
-                    new ExItemStack(Material.CHAINMAIL_LEGGINGS).setUnbreakable(true).setSlot(EquipmentSlot.LEGS),
-                    new ExItemStack(Material.GOLDEN_BOOTS).setUnbreakable(true).setSlot(EquipmentSlot.FEET));
+                    new ExItemStack(Material.CHAINMAIL_HELMET).setUnbreakable(true)
+                            .setSlot(EquipmentSlot.HEAD),
+                    new ExItemStack(Material.GOLDEN_CHESTPLATE).setUnbreakable(true)
+                            .setSlot(EquipmentSlot.CHEST),
+                    new ExItemStack(Material.CHAINMAIL_LEGGINGS).setUnbreakable(true)
+                            .setSlot(EquipmentSlot.LEGS),
+                    new ExItemStack(Material.GOLDEN_BOOTS).setUnbreakable(true)
+                            .setSlot(EquipmentSlot.FEET));
 
 
     public PushUser(Player player) {
@@ -48,7 +51,6 @@ public class PushUser extends GameUser {
         this.setInvulnerable(true);
         this.lockLocation(true);
 
-        this.setSideboard(PushServer.getGameSideboard());
         this.setBossBar(PushServer.getBossBar());
     }
 
