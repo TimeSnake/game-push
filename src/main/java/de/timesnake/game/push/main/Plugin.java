2384a7e3b5b9d13687362fca4685193a5b9d5d58
funzter <funzter@mail.timesnake.de>
2024-05-17 21:41:54 +0200

    minor

--- src/main/java/de/timesnake/game/push/main/Plugin.java
+++ src/main/java/de/timesnake/game/push/main/Plugin.java
@@ -4,16 +4,11 @@
 
 package de.timesnake.game.push.main;
 
-import de.timesnake.library.basic.util.LogHelper;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 public class Plugin extends de.timesnake.basic.game.util.user.Plugin {
 
-  public static final Plugin PUSH = new Plugin("Push", "GPU",
-      LogHelper.getLogger("Push", Level.INFO));
+  public static final Plugin PUSH = new Plugin("Push", "GPU");
 
-  protected Plugin(String name, String code, Logger logger) {
-    super(name, code, logger);
+  protected Plugin(String name, String code) {
+    super(name, code);
   }
 }
